7e4599833cf9122d38a0d7905fc0c17cdc5055d8
NarcHoz95 <axk042@shsu.edu>
2018-07-15 00:32:10 -0400

    From design branch

--- app/src/main/java/com/aranteknoloji/trainingfinalproject/MapsActivity.java
+++ app/src/main/java/com/aranteknoloji/trainingfinalproject/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.aranteknoloji.trainingfinalproject;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,18 +14,51 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+
+    //FloatingActionMenu's action button's initiliaze
+    private FloatingActionButton main, google, list;
+    private int clickCounter = 0;
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setFloationActionButtonMenu();
+    }
+
+    private void setFloationActionButtonMenu() {
+        main = findViewById(R.id.mainActionButton);
+        google = findViewById(R.id.googleMapAction);
+        list = findViewById(R.id.listActionButton);
+        main.setOnClickListener(this);
+        google.setOnClickListener(this);
+        list.setOnClickListener(this);
+    }
+
+    private void setActionsVisible() {
+        Animation animGoogleIn = AnimationUtils.loadAnimation(this, R.anim.google_action_in);
+        Animation animListIn = AnimationUtils.loadAnimation(this, R.anim.list_action_in);
+        google.setAnimation(animGoogleIn);
+        list.setAnimation(animListIn);
+        google.setVisibility(View.VISIBLE);
+        list.setVisibility(View.VISIBLE);
+    }
+
+    private void setActionsGone() {
+        Animation animGoogleOut = AnimationUtils.loadAnimation(this, R.anim.google_action_out);
+        Animation animListOut = AnimationUtils.loadAnimation(this, R.anim.list_action_out);
+        google.setAnimation(animGoogleOut);
+        list.setAnimation(animListOut);
+        google.setVisibility(View.GONE);
+        list.setVisibility(View.GONE);
     }
 
 
@@ -42,5 +79,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mainActionButton:
+                if (clickCounter % 2 == 0) setActionsVisible();
+                else setActionsGone();
+                clickCounter++;
+                break;
+            case R.id.googleMapAction:
+                break;
+            case R.id.listActionButton:
+                break;
+        }
     }
 }
